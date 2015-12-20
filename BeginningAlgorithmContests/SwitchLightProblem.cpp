642abbca3a352ae1777fc0e64785324b4dc318fa
LeongHouHeng <leonghou2001@gmail.com>
2015-12-20 23:33:58 +0800

    BeginningAlgorithmContests

---
+++ BeginningAlgorithmContests/SwitchLightProblem.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#define MAXN 1000 + 10
+using namespace std;
+
+int a[MAXN];
+
+int main(){
+    int i, j, n, k, first = 1;
+    memset(a, 0, sizeof(a));
+    cin >> n >> k;
+    for(i = 1; i <= k; i++)
+        for(j = 1; j <= n; j++)
+            if(j%i == 0)    a[j] = !a[j];
+        
+    for(i = 1; i <= n ;i++){
+        if(a[i]){
+            if(first)
+                first = 0;
+            else
+                cout << " ";
+                
+            cout << i;
+        }
+    }
+    cout << endl;
+    return 0;
+}
